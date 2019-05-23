c9161a5d4cef14d2e40ab74a922188a11c723006
bryantbyr <bryantbyr@163.com>
2019-05-23 16:09:07 +0800

    handle None value / add README.txt

--- algo/parse_sentence_end/EndParser.py
+++ algo/parse_sentence_end/EndParser.py
@@ -24,47 +24,46 @@ def distance(v1, v2):
     return 1 - cosine(v1, v2)
 
 
+# word vector file, can be downloaded from GloVe website
+wordfile = '/home/project-01/PUG/data/wiki_sentences100.txt'
+# each line is a word and its frequency
+weightfile = '/home/project-01/PUG/data/word_cnt.txt'
+# the parameter in the SIF weighting scheme, usually in the range [3e-5, 3e-3]
+weightpara = 1e-3
+rmpc = 1  # number of principal components to remove in SIF weighting scheme
+
+# load word vectors
+(words, We) = data_io.getWordmap(wordfile)
+# load word weights
+# word2weight['str'] is the weight for the word 'str'
+word2weight = data_io.getWordWeight(weightfile, weightpara)
+# weight4ind[i] is the weight for the i-th word
+weight4ind = data_io.getWeight(words, word2weight)
+
+# set parameters
+params = para.params()
+params.rmpc = rmpc
+
+parser = NewsParser('/home/project-01/PUG/data/similar_words.txt')
+
+
 def parse_sentence_end(text):
 
-    # input
-    # word vector file, can be downloaded from GloVe website
-    wordfile = '/home/project-01/PUG/data/wiki_sentences100.txt'
-    # each line is a word and its frequency
-    weightfile = '/home/project-01/PUG/data/word_cnt.txt'
-    # the parameter in the SIF weighting scheme, usually in the range [3e-5,
-    # 3e-3]
-    weightpara = 1e-3
-    rmpc = 1  # number of principal components to remove in SIF weighting scheme
-
-    # load word vectors
-    (words, We) = data_io.getWordmap(wordfile)
-    # load word weights
-    # word2weight['str'] is the weight for the word 'str'
-    word2weight = data_io.getWordWeight(weightfile, weightpara)
-    # weight4ind[i] is the weight for the i-th word
-    weight4ind = data_io.getWeight(words, word2weight)
-
-    # set parameters
-    params = para.params()
-    params.rmpc = rmpc
-
-    parser = NewsParser('/home/project-01/PUG/data/similar_words.txt')
-
     result = []
-
-    text = text.replace('\n','').replace('\r','').replace(' ','')
+    text = text.replace('\n', '').replace('\r', '').replace(' ', '')
 
     while text:
         res = parser.generate(text)
+        if not res:
+            break
         author, start_index_in_text, sen_cuts, start_index = res['speaker'], res['start_index_in_text'], res['sen_cuts'], res['start_index_in_sen_cuts']
 
         sentences = [token(sen) for sen in sen_cuts]
         sentences = [' '.join(s) for s in sentences]
-        sentences = [cut(s) for s in sentences]
+        sentences = [cut(s) for s in sentences if s]
 
         # load sentences
-        # x is the array of word indices, m is the binary mask indicating whether
-        # there is a word in that location
+        # x is the array of word indices, m is the binary mask indicating whether there is a word in that location
         x, m = data_io.sentences2idx(sentences, words)
         w = data_io.seq2weight(x, m, weight4ind)  # get word weights
 
@@ -92,13 +91,14 @@ def parse_sentence_end(text):
         text = text[end:]
         print('remain_text: ', text)
 
+
     return result
 
 
 if __name__ == '__main__':
     with open('test_case.txt', "r") as f:
-        str = f.read()
+        text = f.read()
 
     # parse_sentence_end("特雷莎·梅在《星期日邮报》上发表的一篇文章中声明，让我们听听选民在地方选举中所表达的呼声吧，暂时搁置我们的分歧，达成我们的协议。 据媒体报道，在周四（2日）的英国地方议会选举中，保守党失去了一千多个席位，而计划夺取数百席位的工党亦失去81席。")
-    parse_sentence_end(str)
-
+    # parse_sentence_end("特雷莎·梅在《星期日邮报》上发表的一篇文章中声明，让我们听听选民在地方选举中所表达的呼声吧，暂时搁置我们的分歧，达成我们的协议。 伊朗伊斯兰议会议长拉里·贾尼4日声明：“根据伊核协议，伊朗可以生产重水，这并不违反协议。我们将继续进行铀浓缩活动。”")
+    parse_sentence_end(text)
